354642fa760f2d8938b204577ecffd1cf24495f2
Dinesh Dhanji <56204876+DineshDhanji@users.noreply.github.com>
2020-06-17 20:37:16 +0500

    Add files via upload

---
+++ JAVASCRIPT/01.js
@@ -0,0 +1,43 @@
+function navShow()
+{
+    var a = document.getElementsByClassName("nav-toggle")[0].childNodes[1].innerHTML
+
+    if (a == `<i class="fas fa-bars"></i>`)
+    {
+        document.getElementsByClassName("nav-toggle")[0].childNodes[1].innerHTML = `<i class="fas fa-times"></i>`;
+    }
+    else
+    {
+        document.getElementsByClassName("nav-toggle")[0].childNodes[1].innerHTML = `<i class="fas fa-bars"></i>`;
+    }
+
+    document.getElementsByClassName("nav-toggle")[0].classList.toggle("nav-toggle-btn");
+    document.getElementsByClassName("nav-cont")[0].classList.toggle("nav-cont-show");
+}
+
+function navBarHide()
+{
+    var b = event.target.parentElement.parentElement.parentElement.parentElement.parentElement.classList[0]
+    if (b !== "nav-cont")
+    {
+        event.target.parentElement.parentElement.parentElement.parentElement.parentElement.classList.toggle("nav-bar-hide");
+    }
+    else
+    {
+        event.target.parentElement.parentElement.parentElement.parentElement.classList.toggle("nav-bar-hide");
+    }
+
+    var d = event.target.parentElement.classList[0];
+    if (d !== "nav-head")
+    {
+        event.target.parentElement.classList.toggle("nav-hide-btn");
+    }
+    else
+    {
+        event.target.classList.toggle("nav-hide-btn");
+    }
+
+    
+
+
+}
